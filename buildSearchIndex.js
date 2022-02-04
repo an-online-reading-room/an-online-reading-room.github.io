c9df4adf574001ea74152541955ffc27e85f802e
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-02-04 11:51:34 +0530

    story - strapi integration

--- buildSearchIndex.js
+++ buildSearchIndex.js
@@ -1,15 +1,19 @@
 const createSearchIndex = require('@aha-app/netlify-flexsearch/createSearchIndex');
-const stories = require('./src/_data/_stories')
+const stories = require('./src/_data/stories')()
 
 const searchData = {}
-stories.forEach(story => {
-  searchData[story.id] = {
-    text: story.description,
-    response: {
-      id: story.id
+stories
+.then(data => {
+  data.forEach(story => {
+    searchData[story.id] = {
+      text: story.submission,
+      response: {
+        id: story.id
+      }
     }
-  }
+  })
 })
+
 // console.log(searchData)
 
 createSearchIndex({
