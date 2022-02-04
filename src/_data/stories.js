46726e6611ea315b362e7d645749c361a0cd1c64
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-02-04 11:51:34 +0530

    story - strapi integration

---
+++ src/_data/stories.js
@@ -0,0 +1,36 @@
+const axios = require('axios').default
+const qs = require('qs')
+const slugify = require('../filters/slugify')
+require('dotenv').config()
+
+async function getStories() {
+  try {
+    const res = await axios.get(`${process.env.API_URL}/stories/`, {
+      params: {
+        populate: ['author', 'categories']
+      },
+      paramsSerializer(params) {
+        return qs.stringify(params)
+      },
+    })
+    const stories = res.data.data
+    const data = stories.map(story => {
+      console.log(story.attributes.submission)
+      return {
+        id: story.id,
+        title: story.attributes.title,
+        location: story.attributes.location,
+        submission: story.attributes.submission,
+        author_name: story.attributes.author.data.attributes.username,
+        url: slugify(story.attributes.title)
+      }
+    })
+    // console.log(data)
+    return data
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+getStories()
+module.exports = getStories
