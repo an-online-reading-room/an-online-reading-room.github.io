a78c917a6fad6318022f5b4915db8d5798266e1d
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-02-26 20:14:24 +0530

    map!

---
+++ src/routes/adventure/__layout.svelte
@@ -0,0 +1,15 @@
+
+<script>
+  import Header from '../../components/Header.svelte'
+  import Icon from '../../components/Icon.svelte'
+</script>
+
+<div class="flex flex-col align-items-center gap-y-4
+            w-screen h-screen bg-base
+            text-center">
+  <Header></Header>
+
+  <slot></slot>
+
+</div>
+
---
+++ src/routes/adventure/map/story.svelte
@@ -0,0 +1,26 @@
+
+<script>
+  import Header from '../../../components/Header.svelte';
+  import { onMount } from 'svelte';
+  import "leaflet/dist/leaflet.css"
+  
+  onMount(async () => {
+    const L = (await import('leaflet')).default
+    const geocoder = (await import('pelias-leaflet-plugin')).default
+    
+
+    
+    let map = L.map('map').setView([20.5937, 78.9629], 4);
+    L.tileLayer('https://api.mapbox.com/styles/v1/thereadingroom/ckz5ecvwu000c14pi4cc6z3kr/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoidGhlcmVhZGluZ3Jvb20iLCJhIjoiY2t6NWNicmxlMHAyZzJucW9ydTNrenA0eiJ9.zQ2AECdzKcl5TrQXrGqPeA', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoidGhlcmVhZGluZ3Jvb20iLCJhIjoiY2t6NWNicmxlMHAyZzJucW9ydTNrenA0eiJ9.zQ2AECdzKcl5TrQXrGqPeA'
+    }).addTo(map);
+  })
+</script>
+
+<section id="map" class="h-full">
+</section>
