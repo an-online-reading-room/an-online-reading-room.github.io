fc84d6da77577f38c4fb68881baed4542a1472a6
rndexe <himanshu.ernde@gmail.com>
2022-05-19 15:24:59 +0530

    add signin forms and api utils

---
+++ src/lib/stores/user.js
@@ -0,0 +1,15 @@
+import { writable } from 'svelte/store'
+import { browser } from '$app/env'
+
+const defaultValue = JSON.stringify({})
+const stored = browser ? window.localStorage.user ?? defaultValue : defaultValue
+
+export const user = writable(JSON.parse(stored))
+
+user.subscribe(value => {
+  if(browser) {
+    window.localStorage.setItem('user', JSON.stringify(value))
+  }
+})
+
+export { user as default }
