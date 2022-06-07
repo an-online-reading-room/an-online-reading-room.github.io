fe35495050b8212fa6e9d0ade2f81a3613a35980
rndexe <himanshu.ernde@gmail.com>
2022-06-07 15:48:30 +0530

    editorjs layout changes and clear button

--- src/routes/storyteller/editor.svelte
+++ src/routes/storyteller/editor.svelte
@@ -2,27 +2,19 @@
     import { onMount } from "svelte";
     import { dev } from "$app/env";
     import { getSuggestions } from "../../services/geocode";
-    import { theme } from "$stores/theme";
     import { user } from "$stores/user.js";
     import TopNav from "$components/navigation/TopNav.svelte";
     import Time from "$components/utils/Time.svelte";
+    import Modal from "$components/utils/Modal.svelte";
 
     import * as api from "$lib/api.js";
     import "leaflet/dist/leaflet.css";
 
-    //const API_URL = `${variables.strapi_url}/api`;
-    //let mediaRefs = [];
     export let prevStoryData;
-    console.log(prevStoryData);
+    let isOpenModal = false;
     let form, editor;
-    let locationInput = prevStoryData ? prevStoryData.attributes.location : "";
+    let locationInput = prevStoryData?.attributes.location ?? "";
     let locationSuggestions = [];
-    let uploading = false,
-        done = false;
-    let preview = false,
-        termsCheckbox;
-
-    let story;
 
     $: {
         getSuggestions(locationInput).then(
@@ -31,9 +23,6 @@
     }
 
     async function submitStory() {
-        //preview = false;
-        uploading = true;
-
         const formData = new FormData(form);
 
         const storyData = {
@@ -58,26 +47,31 @@
             }
             res.then((data) => {
                 console.log(data);
-                uploading = false;
-                done = true;
+                isOpenModal = true;
+                //goto("/storyteller");
             });
         });
     }
 
     async function deleteStory() {
-        console.log("deleting ", prevStoryData?.id);
-        const res = await api.del(`api/stories/${prevStoryData.id}`, $user.jwt);
-        console.log(res);
+        //console.log("deleting ", prevStoryData?.id);
+        //const res = await api.del(`api/stories/${prevStoryData.id}`, $user.jwt);
+        //console.log(res);
+        editor.clear();
+        document.getElementById("title").value = ""
+        document.getElementById("location").value = ""
+        document.getElementById("description").value = ""
+
     }
     onMount(async () => {
         const EditorJS = (await import("@editorjs/editorjs")).default;
         const ImageTool = (await import("@editorjs/image")).default;
         editor = new EditorJS({
             holder: "editor",
-            minHeight: 50,
+            minHeight: 120,
             placeholder: "Add your story",
             logLevel: "ERROR",
-            data: prevStoryData ? prevStoryData.attributes.submission : {},
+            data: prevStoryData?.attributes.submission ?? {},
             tools: {
                 image: {
                     class: ImageTool,
@@ -109,82 +103,76 @@
     });
 </script>
 
-<TopNav back="/storyteller" next="" />
-
-<div class="py-3.5 px-8">
-    <Time />
-
-    <form
-        bind:this={form}
-        on:submit|preventDefault={submitStory}
-        enctype="multipart/form-data"
-        id="story"
-        class="space-y-3">
-        <input
-            type="text"
-            name="title"
-            id="title"
-            placeholder="Title"
-            value={prevStoryData ? prevStoryData.attributes.title : ""}
-            required
-            class="text-3xl font-bold focus:outline-none placeholder:text-contrast" />
-        <input
-            bind:value={locationInput}
-            type="text"
-            name="location"
-            id="location"
-            placeholder="Add Location"
-            required
-            list="location-suggestions"
-            class="text-sm focus:outline-none placeholder:text-contrast" />
-        <datalist id="location-suggestions">
-            {#each locationSuggestions as suggestion}
-                <option value={suggestion}> {suggestion} </option>
-            {/each}
-        </datalist>
-
-        <textarea
-            name="description"
-            id="desciption"
-            placeholder="Add Summary"
-            value={prevStoryData ? prevStoryData.attributes.description : ""}
-            maxlength="400"
-            required
-            class="focus:outline-none placeholder:font-bold placeholder:text-contrast" />
-    </form>
-    <section class="placeholder:text-contrast" id="editor" />
-    <div class="flex flex-col fixed inset-x-0 bottom-0 z-10 bg-primary">
-        <label class="self-center my-1 font-text text-contrast text-xs">
+<div class="overflow-y-auto">
+    <TopNav back="/storyteller" next="" />
+    <main class="py-3.5 px-8">
+        <Time />
+        <form
+            bind:this={form}
+            on:submit|preventDefault={submitStory}
+            enctype="multipart/form-data"
+            id="story"
+            class="space-y-3">
             <input
-                class="accent-accent w-2.5 h-2.5"
-                type="checkbox"
-                name="accept"
-                form="story"
-                required />
-            I accept the
-            <a class="underline" href="/terms-and-conditions">
-                terms and conditions</a>
-        </label>
-        <div class="flex h-11 bg-accent text-primary">
-            <button class="w-1/2" type="submit" form="story">Publish</button>
-            <button class="w-1/2" on:click={deleteStory}>Delete</button>
-        </div>
-    </div>
-    {#if uploading}
-        <div class="flex-1 flex flex-col justify-center">
-            <video src="/img/loading-{$theme}.webm" autoplay loop muted />
-        </div>
-    {/if}
+                type="text"
+                name="title"
+                id="title"
+                placeholder="Title"
+                value={prevStoryData?.attributes.title ?? ""}
+                required
+                class="text-3xl font-bold focus:outline-none placeholder:text-contrast" />
+            <input
+                bind:value={locationInput}
+                type="text"
+                name="location"
+                id="location"
+                placeholder="Add Location"
+                required
+                list="location-suggestions"
+                class="text-sm focus:outline-none placeholder:text-contrast" />
+            <datalist id="location-suggestions">
+                {#each locationSuggestions as suggestion}
+                    <option value={suggestion}> {suggestion} </option>
+                {/each}
+            </datalist>
 
-    {#if done}
-        <section
-            class="flex-1 flex flex-col justify-center gap-y-6 font-text text-sm text-contrast">
-            <p>Thanks for submitting!</p>
-            <!--<a href="/stories/{story.url}" class="underline"
-                >View your story here</a>-->
-        </section>
-    {/if}
+            <textarea
+                name="description"
+                id="description"
+                placeholder="Add Summary"
+                value={prevStoryData?.attributes.description ?? ""}
+                maxlength="400"
+                required
+                class="focus:outline-none placeholder:font-bold placeholder:text-contrast" />
+        </form>
+        <section class="placeholder:text-contrast" id="editor" />
+        <div class="fixed inset-x-0 bottom-0 z-10 flex flex-col bg-primary">
+            <label class="self-center my-2 font-text text-contrast text-xs">
+                <input
+                    class="accent-accent w-2.5 h-2.5"
+                    type="checkbox"
+                    name="accept"
+                    form="story"
+                    required />
+                I accept the
+                <a class="underline" href="/terms-and-conditions">
+                    terms and conditions</a>
+            </label>
+            <div class="flex h-11 bg-accent text-primary">
+                <button class="w-1/2" type="submit" form="story"
+                    >Publish</button>
+                <button class="w-1/2" on:click={deleteStory}>Clear</button>
+            </div>
+        </div>
+    </main>
 </div>
+
+<Modal {isOpenModal} showCloseButton={false}>
+    Thanks for submitting!
+    <a href="/storyteller">
+        <p class="underline font-bold">Go back</p>
+    </a>
+</Modal>
 
 <style lang="postcss">
     #story input {
@@ -207,5 +195,8 @@
     }
     :global(.cdx-input) {
         @apply text-left text-xs p-0;
+    }
+    :global(.ce-toolbar) {
+        @apply bg-primary shadow-none border border-contrast;
     }
 </style>
