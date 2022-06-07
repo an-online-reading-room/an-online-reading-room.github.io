fbba115b82bab78ad442398df4eabb5653182fcf
rndexe <himanshu.ernde@gmail.com>
2022-06-07 18:58:03 +0530

    add delete button

--- src/routes/storyteller/index.svelte
+++ src/routes/storyteller/index.svelte
@@ -2,9 +2,14 @@
     import * as api from "$lib/api.js";
     import { onMount } from "svelte";
     import { user } from "$stores/user.js";
+    import Delete from "$components/icons/Delete.svelte";
+    import Modal from "$components/utils/Modal.svelte";
 
-    let stories;
-    onMount(async () => {
+    let stories = [];
+    let isOpenModal = false;
+    onMount(getStories);
+
+    async function getStories() {
         //console.log($user.jwt)
         try {
             const userData = await api.get("api/users/me", $user.jwt);
@@ -12,10 +17,23 @@
         } catch (error) {
             console.error(error);
         }
-    });
+    }
+    async function deleteStory(id) {
+        await api.del(`api/stories/${id}`, $user.jwt);
+        getStories();
+        closeDeleteModal();
+    }
+
+    function openDeleteModal() {
+        isOpenModal = true;
+    }
+    function closeDeleteModal() {
+        isOpenModal = false;
+    }
 </script>
 
-<div class="overflow-y-auto flex-column gap-y-2 px-8 py-3.5 pb-12 border-t border-contrast">
+<div
+    class="overflow-y-auto flex-column gap-y-2 px-8 py-3.5 pb-12 border-t border-contrast">
     <a href="/storyteller/editor">
         <div class="btn">Create a new story</div>
     </a>
@@ -26,11 +44,22 @@
         {#each stories as story}
             <a
                 href="storyteller/editor?story={story.id}"
-                class="flex flex-col gap-y-3 border-2 border-contrast px-3.5 py-2 font-display text-contrast">
+                class="relative flex flex-col gap-y-3 border-2 border-contrast px-3.5 py-2 font-display text-contrast">
+                <button on:click|preventDefault={openDeleteModal} class="absolute top-3 right-2">
+                    <Delete />
+                </button>
                 <p class="text-base font-medium">{story.title}</p>
                 <p class="text-sm font-text">{story.location}</p>
                 <p class="text-sm font-text">{story.description}</p>
             </a>
+            <Modal {isOpenModal} showCloseButton={false}>
+                <p>Are you sure you want to delete this story?</p>
+                <div class="flex justify-around mt-6">
+                <button class="btn btn-modal" on:click={deleteStory(story.id)}>Yes</button>
+                <button class="btn btn-modal" on:click={closeDeleteModal}>No</button>
+            </div>
+
+            </Modal>
         {/each}
     {/if}
 </div>
@@ -38,5 +67,8 @@
 <style lang="postcss">
     .btn {
         @apply bg-accent text-primary text-sm px-4 py-3;
+    }
+    .btn-modal {
+        @apply w-16 h-fit border border-primary px-4 py-2;
     }
 </style>
