ed23100d7fd0bd56ca746b57f67917bd98257bd3
rndexe <himanshu.ernde@gmail.com>
2022-06-07 18:58:03 +0530

    add delete button

---
+++ src/components/icons/Delete.svelte
@@ -0,0 +1,11 @@
+<svg
+    class="w-auto h-4"
+    viewBox="0 0 15 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M10 0H5V1.25H10V0ZM0 2.5V3.75H1.25V16.25C1.25 16.9404 1.80964 17.5 2.5 17.5H12.5C13.1904 17.5 13.75 16.9404 13.75 16.25V3.75H15V2.5H0ZM2.5 16.25V3.75H12.5V16.25H2.5ZM5 6.25H6.25V13.75H5V6.25ZM10 6.25H8.75V13.75H10V6.25Z"
+        class="fill-contrast" />
+</svg>
