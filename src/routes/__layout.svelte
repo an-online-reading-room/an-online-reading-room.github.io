1d0f6d24163d0864efd2c1260cb5b5a36a0d5995
rndexe <himanshu.ernde@gmail.com>
2022-06-07 16:11:15 +0530

    message for desktop

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,7 +5,10 @@
 </script>
 
 <div class={"h-full theme-" + $theme}>
-    <div class="h-full font-display bg-primary">
+    <div class="lg:hidden h-full font-display bg-primary">
         <slot />
+    </div>
+    <div class="hidden h-full lg:block text-center font-text pt-56 bg-primary">
+        The Reading Room is only viewable on phones and tablets. See you there!
     </div>
 </div>
