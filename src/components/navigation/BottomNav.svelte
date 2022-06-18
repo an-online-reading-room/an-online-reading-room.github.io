dcb18111807aed1b226875a45492257b7597c6d4
rndexe <himanshu.ernde@gmail.com>
2022-06-18 17:06:58 +0530

    ui changes for modal and bottomnav

--- src/components/navigation/BottomNav.svelte
+++ src/components/navigation/BottomNav.svelte
@@ -1,17 +1,11 @@
-<div class="flex flex-col absolute inset-x-0 bottom-0 gap-y-2">
-    <label class="self-center font-text text-xs">
-        <input
-            class="accent-accent w-2.5 h-2.5"
-            type="checkbox"
-            name="accept"
-            form="story"
-            required />
-        I accept the
-        <a class="underline" href="/terms-and-conditions">
-            terms and conditions</a>
-    </label>
-    <div class="h-11 bg-accent text-primary">
-        <button type="submit" form="story">Publish</button>
-        <button>Delete</button>
+<script>
+    export let faded = false;
+</script>
+<div class="fixed inset-x-0 bottom-0 z-10 flex-column gap-y-2 bg-primary">
+    {#if !faded}
+    <slot name="editor-extras"></slot>
+    {/if}
+    <div class="flex h-11 bg-accent text-primary" class:opacity-75={faded}>
+        <slot name="bottom-bar"></slot>
     </div>
 </div>
