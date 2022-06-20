c84507e5bd8d3f7b6f4968566829a11af19af285
rndexe <himanshu.ernde@gmail.com>
2022-06-20 17:16:23 +0530

    cracking modals

---
+++ src/routes/storyteller/_modals/Clear.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+
+    function clear() {
+        dispatch("clear");
+    }
+
+    function close() {
+        dispatch("close");
+    }
+</script>
+<p class="font-bold pr-5">Are you sure you want to start over?</p>
+<p>We’ll miss it!</p>
+<div class="flex justify-between mt-3">
+    <button class="btn-modal" on:click={clear}>Yes</button>
+    <button class="btn-modal" on:click={close}>No</button>
+</div>
+
+
+<style lang="postcss">
+    .btn-modal {
+        @apply bg-primary text-accent font-display font-bold text-sm h-fit px-8 py-2;
+    }
+</style>
