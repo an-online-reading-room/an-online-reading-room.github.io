7cbde31272622d3bc180e393636648a802b18f5d
rndexe <himanshu.ernde@gmail.com>
2022-06-21 16:46:37 +0530

    remove published page

--- src/components/navigation/BottomNav.svelte
+++ src/components/navigation/BottomNav.svelte
@@ -1,11 +1,7 @@
-<script>
-    export let faded = false;
-</script>
+
 <div class="fixed inset-x-0 bottom-0 z-10 flex-column gap-y-2 bg-primary">
-    {#if !faded}
     <slot name="editor-extras"></slot>
-    {/if}
-    <div class="flex h-11 bg-accent text-primary" class:opacity-75={faded}>
+    <div class="flex h-11 bg-accent text-primary">
         <slot name="bottom-bar"></slot>
     </div>
 </div>
