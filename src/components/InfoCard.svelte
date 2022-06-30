4b6ba3f771b31f3fbf0d436c0ef2bf4ab10daee0
Anushka Trivedi <anushka@mimaze.co.jp>
2022-06-30 14:28:56 +0900

    #7 - direction calculation, adventure story page info card, faq page, removed linker from lite story page

---
+++ src/components/InfoCard.svelte
@@ -0,0 +1,23 @@
+<script>
+
+
+export let open
+export let info
+
+</script>
+  
+{#if open}
+<div
+    class="z-10 absolute inset-x-0 bottom-12
+                bg-accent text-primary text-base
+                py-6 px-10">
+    <div class="flex flex-col gap-y-2 text-left">
+
+        <h1 class="font-display font-bold text-base">{info.title}</h1>
+        <p class="font-text text-xs">by {info.users_permissions_user?.data.attributes.username}</p>
+        <p class="font-text text-sm">{info.location}</p>
+        <p class="font-display text-sm">{info.description}</p>
+
+    </div>
+</div>
+{/if}
--- src/routes/travelling/[slug].svelte
+++ src/routes/travelling/[slug].svelte
@@ -1,11 +1,67 @@
+<script context="module">
+
+export async function load({ params, session }) {
+
+  const query = qs.stringify({
+    stories: {
+      fields: ['location'],
+      filters: {
+        slug: { $eq: params.slug }
+      }
+    }
+  }, {
+    encodeValuesOnly: true
+  })
+  const res = await api.get(
+    `api/stories?${query}`,
+    get(user).jwt
+  )
+  const data = flattenStrapiResponse(res)[0]
+
+  return {
+    status: 200,
+    props: {
+      pointA: session.pointA,
+      pointB: {
+        slug: params.slug,
+        location: data.location
+      }
+    }
+  }
+
+}
+
+</script>
+
 <script>
 import { goto } from "$app/navigation";
 import { page } from "$app/stores"
+import qs from 'qs'
+import * as api from '$lib/api'
+import { getCoordinates } from "$lib/services/geocode";
 import { onMount } from "svelte";
+import { get } from "svelte/store";
+import user from "$stores/user";
+import { flattenStrapiResponse } from "$lib/utils/api";
 
 const target = $page.params.slug
+export let pointA
+export let pointB 
 
-onMount(() => {
+onMount(async () => {
+  pointA.coordinates = await getCoordinates(pointA.location)
+  pointB.coordinates = await getCoordinates(pointB.location)
+  
+  const L = (await import('leaflet')).default
+  let lpointA = L.point(pointA.coordinates.x, pointA.coordinates.y)
+  let lpointB = L.point(pointB.coordinates.x, pointB.coordinates.y)
+
+  let dir = lpointB.subtract(lpointA)
+  let dirLength = Math.sqrt(Math.pow(dir.x, 2) + Math.pow(dir.y, 2)) 
+  let dirNorm = L.point(dir.x/dirLength, dir.y/dirLength)
+  console.log("direction :", dir)
+  console.log("direction normalized: ", dirNorm)
+
   setTimeout(() => {
     goto(`/adventure/read?story=${target}`)
   }, 2000)
