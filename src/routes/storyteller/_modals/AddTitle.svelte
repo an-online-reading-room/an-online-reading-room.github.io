a41dc8e1eb77cc914ef1cb11689e98cefd52248a
rndexe <himanshu.ernde@gmail.com>
2022-07-02 23:38:34 +0530

    add title popup

---
+++ src/routes/storyteller/_modals/AddTitle.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import CloseButton from "$components/icons/CloseButton.svelte";
+
+    const dispatch = createEventDispatcher();
+
+    function close() {
+        dispatch("close");
+    }
+</script>
+<p class="font-bold pr-4">Please add a title to your story</p>
+<button class="absolute top-3 right-2" on:click={close}>
+    <CloseButton/>
+</button>
