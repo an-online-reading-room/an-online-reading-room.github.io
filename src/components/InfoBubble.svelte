463afdd419c785eeac8708907810e8be208ff767
Anushka Trivedi <21999998+opheliagame@users.noreply.github.com>
2022-07-11 14:47:18 +0900

    share buttons implementation - except instagram

---
+++ src/components/InfoBubble.svelte
@@ -0,0 +1,17 @@
+<script>
+  
+export let open = false
+
+</script>
+
+
+{#if open}
+<div class="z-100 absolute top-0 left-0 w-full h-full">
+  <div class="absolute bottom-16 left-8 w-full 
+  flex justify-center">
+    <button class="bg-primary rounded-full p-2">
+      <p class="text-accent text-sm">link copied!</p>
+    </button>
+  </div>
+</div>
+{/if}
